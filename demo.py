7945c32a74dccaa70893b746318279e0d1baa023
Harshith <tallojarshith123@gmail.com>
2024-05-01 17:10:57 +0530

    utils added

--- demo.py
+++ demo.py
@@ -1,2 +1,9 @@
 from us_visa.logger import logging
-logging.info("welcome to our custom log")
+from us_visa.exception import USvisaException
+import sys
+
+try:
+    a=1/"10"
+except Exception as e:
+    logging.info(e)
+    raise USvisaException(e,sys) from e
